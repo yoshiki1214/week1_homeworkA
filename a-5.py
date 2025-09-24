eb4c216a13d48b8938046265353c1cf5b833ea1e
yoshiki1214 <gazerk43@gmail.com>
2025-09-24 21:48:31 +0900

    課題A修正

--- a-5.py
+++ a-5.py
@@ -1,5 +1,3 @@
 bob_info = ["Bob", "Dylan", 79]
 
-fullname = bob_info[0]
-
 print(f"Name: {bob_info[0]} {bob_info[1]},Age: {bob_info[2]}")
--- a-7.py
+++ a-7.py
@@ -1,4 +1,4 @@
 even_numbers = [2, 4, 6, 8]
 
 for i in even_numbers:
-    print(i ** 2)
+    print(i * 2)
